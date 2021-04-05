c4eb5383560e024ea5075ed6312af1e530727547
pal2097 <pal2097@gmail.com>
2021-04-05 09:53:51 +0800

    新增LogSender

---
+++ AkkaWebTemplate/LogSender/ILog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogSender
+{
+    public interface ILog
+    {
+        void E(string title, string content);
+        void A(string title, string contetnt);
+        void I(string title, string content);
+        void D(string title, string content);
+    }
+}
